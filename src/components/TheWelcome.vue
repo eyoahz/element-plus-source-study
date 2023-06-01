a9238ccbfd4f5cdd4da3a6dde84f61e21a463601
zhaoye <122477559@qq.com>
2023-06-01 17:57:33 +0800

    feat: 初始化

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref<Date>(new Date())
+</script>
+
+<template>
+  <view>
+    <el-calendar v-model="value" />
+  </view>
+</template>
